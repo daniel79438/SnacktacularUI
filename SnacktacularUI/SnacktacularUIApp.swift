7e5ae14a99922a9182b7a0dc8612c5b127348e34
daniel79438 <daniel79438@gmail.com>
2025-04-05 13:55:15 +0100

    Initial Commit

---
+++ SnacktacularUI/SnacktacularUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularUIApp.swift
+//  SnacktacularUI
+//
+//  Created by Daniel Harris on 05/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
